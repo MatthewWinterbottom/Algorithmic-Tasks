24a7c265d1e6d93dd859497372b033c8c457358c
Matthew Winterbottom <matthewwinterbottom@outlook.com>
2019-11-18 11:05:54 +0000

    initialise project

---
+++ algorithm-solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithm_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
